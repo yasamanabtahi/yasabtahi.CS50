e1c9006eee864a2d9cae44ca13b4e7e5ab397d66
yasamanabtahi <164859336+yasamanabtahi@users.noreply.github.com>
2024-03-29 00:40:18 +0330

    Add files via upload

---
+++ interpreter.py
@@ -0,0 +1,16 @@
+x,y,z=input("Expression: ").split(" ")
+
+if y== "+":
+    Answer = float(x) + float(z)
+    print(Answer)
+elif y== "-":
+    Answer = float(x) - float(z)
+    print(Answer)
+elif y== "*":
+    Answer = float(x) * float(z)
+    print(Answer)
+elif y== "/":
+    Answer = float(x) / float(z)
+    print(Answer)
+
+    
